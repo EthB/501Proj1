f70b34634292a1635f5dbd46f77eb6b4e6957847
EthB <31282056+EthB@users.noreply.github.com>
2018-04-25 18:00:45 -0500

    Add files via upload
    Added Files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _501Project1_EB
+{
+    /// <summary>
+    /// main class to handle startup, arguments,
+    /// and continued operation of the rest of the sales system
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// SalesManager class that will run the sales system
+        /// </summary>
+        private static SalesManager SalesManager = new SalesManager();
+        /// <summary>
+        /// main startup method handles startup and continued operation of the sales system
+        /// </summary>
+        /// <param name="args">startup args</param>
+        static void Main(string[] args)
+        {
+            //SalesManager SalesManager = new SalesManager();
+            while (true) { SalesManager.Sale(); }
+            
+        }
+    }
+}
